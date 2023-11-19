ea4988ac32df293ea83619f5b1bd9df978f70880
hayyi2 <ahmadhayyi2@gmail.com>
2023-11-19 17:35:18 +0700

    github action build & deploy gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/shadcn-sample/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
